7841fdc13ccd214c12942872c7ad9d2d55b0e560
Oliver Nordbjerg <hi@notbjerg.me>
2017-12-18 16:24:36 +0100

    Add intent handler

--- src/rpc/handlers/intent.js
+++ src/rpc/handlers/intent.js
@@ -1,11 +1,20 @@
-export default function (request, proxy) {
-  // TODO: Is this a smell?
+export default async function (request, proxy) {
+  // TODO(onbjerg): Is this a smell?
   const transactionQueue = proxy.getWrapper().transactions
 
+  // Get the transaction path
+  const acl = proxy.getWrapper().acl()
+  const path = await acl.getTransactionPath(
+    proxy.address,
+    request.params[0],
+    request.params.slice(1)
+  )
+
   return new Promise((resolve, reject) => {
+    // Push the transaction to the wrapper
     transactionQueue.next({
-      transaction: null,
-      path: [],
+      transaction: path[0],
+      path,
       accept (transactionHash) {
         resolve(transactionHash)
       },
