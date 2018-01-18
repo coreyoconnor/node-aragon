e2f7c2454313e3ae253260dc71ddc993ce3b1453
Oliver Nordbjerg <hi@notbjerg.me>
2018-01-18 20:06:23 +0100

    Add provider test helper

--- test/helpers/provider.js
+++ test/helpers/provider.js
@@ -1,11 +1,18 @@
 import ganache from 'ganache-core'
+import Web3 from 'web3'
 
-export default (gasLimit = 50000000) => (t) => {
+export default (gasLimit = 50000000) => async (t) => {
   // Set up provider w/ monkey patch needed for Web3 1.0.0
-  const provider = ganache.provider({
+  let provider = ganache.provider({
     gasLimit
   })
-  provider.send = provider.sendAsync
+  provider = Object.assign(
+    provider,
+    { send: provider.sendAsync.bind(provider) })
+
+  const web3 = new Web3(provider)
 
   t.context.provider = provider
+  t.context.web3 = web3
+  t.context.accounts = await web3.eth.getAccounts()
 }
